5b6b7f2e6058dcc0455212a166fa13285b79d2fe
asiftm <113905560+asiftm@users.noreply.github.com>
2024-04-25 07:30:15 +0200

    a

---
+++ frontend/public/homepage/script.js
@@ -0,0 +1,7 @@
+document.getElementById("login-btn").addEventListener("click", function() {
+  document.getElementById("login-form-container").style.display = "block";
+});
+
+document.getElementById("close-btn").addEventListener("click", function() {
+  document.getElementById("login-form-container").style.display = "none";
+});
