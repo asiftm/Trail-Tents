9bd4d26027971850afe4e8ae5f1cbf7e3b2e5824
Asif Mahmud <r0918559@student.thomasmore.be>
2024-04-26 12:30:38 +0200

    aaa

--- backend/TrailTents/Database/AnonymousDataContext.cs
+++ backend/TrailTents/Database/AnonymousDataContext.cs
@@ -4,15 +4,17 @@ namespace TrailTents.Database
 {
     public class AnonymousDataContext:IAnonymousDataContext
     {
+        
+
         // Admin
+        Admin admin = new Admin();
         public List<Admin> GetAllAdmins()
         {
-            Admin admin = new Admin();
+            
             return admin.GetAllAdmins();
         }
         public Admin GetAdminByID(int id)
         {
-            Admin admin = new Admin();
             return admin.GetAdminByID(id);
         }
         public bool AddAdmin(Admin admin)
@@ -26,14 +28,13 @@ namespace TrailTents.Database
 
 
         // Bookings
+        Booking booking = new Booking();
         public List<Booking> GetAllBookings()
         {
-            Booking booking = new Booking();
             return booking.GetAllBookings();
         }
         public Booking GetBookingByID(int id)
         {
-            Booking booking = new Booking();
             return booking.GetBookingByID(id);
         }
         public bool AddBooking(Booking booking)
@@ -43,14 +44,13 @@ namespace TrailTents.Database
 
 
         // Camping Site
+        CampingSite campingSite = new CampingSite();
         public List<CampingSite> GetAllCampingSites()
         {
-            CampingSite campingSite = new CampingSite();
             return campingSite.GetAllCampingSites();
         }
         public CampingSite GetCampingSiteByID(int id)
         {
-            CampingSite campingSite = new CampingSite();
             return campingSite.GetCampingSiteByID(id);
         }
         public bool AddCampingSite(CampingSite campingSite)
@@ -64,14 +64,13 @@ namespace TrailTents.Database
 
 
         // Review
+        Review review = new Review();
         public List<Review> GetAllReviews()
         {
-            Review review = new Review();
             return review.GetAllReviews();
         }
         public Review GetReviewByID(int id)
         {
-            Review review = new Review();
             return review.GetReviewByID(id);
         }
         public bool AddReview(Review review)
@@ -80,25 +79,22 @@ namespace TrailTents.Database
         }
         public List<Review> GetReviewsByUserID(int id)
         {
-            Review review = new Review();
             return review.GetReviewsByUserID(id);
         }
         public List<Review> GetReviewsByCampingSiteID(int id)
         {
-            Review review = new Review();
             return review.GetReviewsByCampingSiteID(id);
         }
 
 
         // User
+        User user = new User();
         public List<User> GetAllUsers()
         {
-            User user = new User();
             return user.GetAllUsers();
         }
         public User GetUserByID(int id)
         {
-            User user= new User();
             return user.GetUserByID(id);
         }
         public bool AddUser(User user)
