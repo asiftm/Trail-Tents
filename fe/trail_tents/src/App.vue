2c7e26ef13562e70af0c922a851dea038da167f6
Asif Mahmud <r0918559@student.thomasmore.be>
2024-05-15 21:56:23 +0200

    a

---
+++ fe/trail_tents/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <Login />
+  </div>
+</template>
+
+<script>
+import Login from './components/Login.vue'
+export default {
+  name: 'App',
+  components:{
+    Login
+  }
+}
+</script>
---
+++ fe/trail_tents/src/components/SignUp.vue
@@ -0,0 +1,60 @@
+<template>  
+  <div class="content">
+  <div class="text">
+    Signup Form
+  </div>
+  <form action="#">
+    <div class="field">
+      <input type="text" required>
+      <span class="fas fa-user"></span>
+      <label>Firstname</label>
+    </div>
+    <div class="field">
+      <input type="text" required>
+      <span class="fas fa-user"></span>
+      <label>Lastname</label>
+    </div>
+    <div class="field">
+      <input type="text" required>
+      <span class="fas fa-user"></span>
+      <label>Date Of Birth</label>
+    </div>
+    <div class="field">
+      <input type="text" required>
+      <span class="fas fa-user"></span>
+      <label>Email</label>
+    </div>
+    <div class="field">
+      <input type="text" required>
+      <span class="fas fa-user"></span>
+      <label>Address</label>
+    </div>
+    <div class="field">
+      <input type="text" required>
+      <span class="fas fa-user"></span>
+      <label>Contact Number</label>
+    </div>
+    <div class="field">
+      <input type="password" required>
+      <span class="fas fa-lock"></span>
+      <label>Password</label>
+    </div>
+    <div class="field">
+      <input type="password" required>
+      <span class="fas fa-lock"></span>
+      <label>Password</label>
+    </div>
+    <button>Sign in</button>
+    <div class="sign-up">
+      Already a member?
+      <a href="#">signup now</a>
+    </div>
+  </form>
+</div>
+</template>
+<script>
+import '../assets/login.css';
+export default {
+  name: 'Login'
+}
+</script>
