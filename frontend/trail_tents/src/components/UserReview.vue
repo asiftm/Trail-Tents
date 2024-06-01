063b67722acc6ee6466e3e8773f8b13610d6ea74
Asif Mahmud <r0918559@student.thomasmore.be>
2024-06-01 04:14:09 +0200

    a

--- frontend/trail_tents/src/components/UserReview.vue
+++ frontend/trail_tents/src/components/UserReview.vue
@@ -5,20 +5,16 @@
     </p>
     <div class="review-container">
       <div class="review" v-for="(item, index) in reviewList" :key="index">
-        <div id="comment">
+        <div class="review-item" id="campsiteName">
+          <button>{{ item.campsiteName }}</button>
+        </div>
+        <div class="review-item" id="comment">
           {{ item.comment }}
         </div>
-        <div id="rating">
-          {{ item.rating }}
-        </div>
-        <div id="campsiteName">
-          {{ item.campsiteName }}
-        </div>
-        <div id="campsiteName">
-          {{ item.campsiteID }}
-        </div>
-        <div id="campsiteName">
-          {{ item.campsiteName }}
+        <div class="review-item" id="rating">
+          <div class="rating-star-container" v-for="(i, index) in item.rating" :key="index">
+            <img src="../assets/1149472-removebg-preview.png" alt="">
+          </div>
         </div>
       </div>
     </div>
@@ -26,49 +22,47 @@
 </template>
 
 <script>
-import axios from "axios";
+// import axios from "axios";
 
 export default {
   name: "UserReview",
   data() {
     return {
       user_id: null,
-      reviewList: [],
-      campsite_name: "gh",
-      campsite_id: 0
+      reviewList: []
     }
   },
   methods: {
-    async GetUserReviews() {
-      try {
-        let result = await axios.get(`https://localhost:5272/User/${this.user_id}/Reviews`);
-        if (result.status == 200 && result.data != null) {
-          for (let i = 0; i < result.data.length; i++) {
-            this.campsite_id = result.data[i].campsiteID
-            await this.GetCampsite()
-            let dataDict = {
-              comment: result.data[i].comment,
-              rating: result.data[i].rating,
-              campsiteID: result.data[i].campsiteID,
-              campsiteName: this.campsite_name
-            }
-            this.reviewList.push(dataDict);
-          }
-        }
-      } catch (error) {
-        console.log(error)
-      }
-    },
-    async GetCampsite() {
-      try {
-        let result = await axios.get(`https://localhost:5272/CampingSite/${this.campsite_id}`);
-        if (result.status == 200 && result.data != null) {
-          this.campsite_name = result.data.name
-        }
-      } catch (error) {
-        console.log(error)
-      }
-    },
+    // async GetUserReviews() {
+    //   try {
+    //     let result = await axios.get(`https://localhost:5272/User/${this.user_id}/Reviews`);
+    //     if (result.status == 200 && result.data != null) {
+    //       for (let i = 0; i < result.data.length; i++) {
+    //         let campsitedata = await this.GetCampsite(result.data[i].campsiteID)
+    //         let dataDict = {
+    //           comment: result.data[i].comment,
+    //           rating: result.data[i].rating,
+    //           campsiteID: result.data[i].campsiteID,
+    //           campsiteName: campsitedata.name
+    //         }
+    //         this.reviewList.push(dataDict);
+    //         console.log(typeof(dataDict.rating))
+    //       }
+    //     }
+    //   } catch (error) {
+    //     console.log(error)
+    //   }
+    // },
+    // async GetCampsite(id) {
+    //   try {
+    //     let result = await axios.get(`https://localhost:5272/CampingSite/${id}`);
+    //     if (result.status == 200 && result.data != null) {
+    //       return result.data
+    //     }
+    //   } catch (error) {
+    //     console.log(error)
+    //   }
+    // },
   },
   mounted() {
     let userInfo = localStorage.getItem("userInfo");
@@ -77,7 +71,21 @@ export default {
     } else {
       const user = JSON.parse(userInfo);
       this.user_id = user.id;
-      this.GetUserReviews(this.user_id);
+      // this.GetUserReviews(this.user_id);
+      let dataDict = {
+        comment: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like). Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        rating: 5,
+        campsiteID: 1,
+        campsiteName: "Natures queen"
+      }
+      this.reviewList.push(dataDict);
+      dataDict = {
+        comment: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like). Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+        rating: 3,
+        campsiteID: 1,
+        campsiteName: "Natures queen"
+      }
+      this.reviewList.push(dataDict);
     }
   },
 }
@@ -97,14 +105,53 @@ export default {
   font-weight: 600;
 }
 
-.review-container {}
+.main button{
+  background: none;
+  padding-bottom: 5px;
+  box-shadow: none;
+}
+
+.review-container {
+
+}
 
 .review {
   height: auto;
   width: 600px;
   border-radius: 4px;
-  margin-top: 20px;
+  margin-bottom: 30px;
   box-shadow: 0 5px 10px rgba(0, 0, 0, 0.3);
   background-color: #f0f0f0;
+  position: relative;
+}
+
+.review-item {
+  padding: 5px;
+}
+
+
+#rating {
+  padding: 0;
+  display: flex;
+  flex-direction: row;
+  position: absolute;
+  top: -15px;
+  right: 0;
+}
+
+.rating-star-container {
+  display: flex;
+}
+
+.rating-star-container img {
+  object-fit: cover;
+  height: 30px;
+  width: 30px;
+  padding: 0px 5px;
+}
+
+#campsiteName {
+  font-size: 20px;
+  font-weight: 600;
 }
 </style>
